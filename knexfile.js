1df8015675ff41c7ba0806551ffb6cad1263ea8d
Patrick Goulding <patrickgoulding@gmail.com>
2019-08-16 10:53:52 -0600

    add seed directories to production

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection:  'postgres://localhost/drinks',
+    seeds: {
+      directory: './seeds/recipes'
+    },
     migrations: {
       directory: './migrations'
     },
@@ -14,6 +17,9 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
+    seeds: {
+      directory: './seeds/recipes'
+    },
     migrations: {
       directory: './migrations'
     },
