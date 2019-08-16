567573f31416b3d5f55df54f9a23883a7347ef96
Patrick Goulding <patrickgoulding@gmail.com>
2019-08-16 09:02:40 -0600

    example seed file

---
+++ exampleSeeds.js
@@ -0,0 +1,30 @@
+export const exampleRecipes  = [
+  {
+    name: 'Gin and Juice',
+    category: 'snoop dog drinks',
+    glass: 'Pimp cup',
+    instructions: 'combine gin and juice while rolliing down the street, smoke an indo, sip responsibly.',
+    image: 'https://i.imgur.com/yMg6Z5w.jpg'
+  },
+  {
+    name: 'Gin and Juice',
+    category: 'snoop dog drinks',
+    glass: 'Pimp cup',
+    instructions: 'combine gin and juice while rolliing down the street, smoke an indo, sip responsibly.',
+    image: 'https://i.imgur.com/yMg6Z5w.jpg'
+  },
+  {
+    name: 'Gin and Juice',
+    category: 'snoop dog drinks',
+    glass: 'Pimp cup',
+    instructions: 'combine gin and juice while rolliing down the street, smoke an indo, sip responsibly.',
+    image: 'https://i.imgur.com/yMg6Z5w.jpg'
+  },
+  {
+    name: 'Gin and Juice',
+    category: 'snoop dog drinks',
+    glass: 'Pimp cup',
+    instructions: 'combine gin and juice while rolliing down the street, smoke an indo, sip responsibly.',
+    image: 'https://i.imgur.com/yMg6Z5w.jpg'
+  }
+]
