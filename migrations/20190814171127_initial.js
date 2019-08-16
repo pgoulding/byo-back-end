5a2c8726b44ad90087717104119cbd55df3179c2
Patrick Goulding <patrickgoulding@gmail.com>
2019-08-16 08:59:53 -0600

    add join table

--- migrations/20190814171127_initial.js
+++ migrations/20190814171127_initial.js
@@ -5,19 +5,23 @@ exports.up = function(knex) {
       table.increments('id').primary()
       table.string('name')
       table.string('category')
+      table.text('instructions')
       table.string('glass')
-      table.string('instructions')
       table.string('image')
       table.timestamps(true, true)
     }),
     knex.schema.createTable('recipe_ingredients', table => {
       table.increments('id').primary()
-
+      table.string('amount')
+      table.integer('recipe_id').unsigned()
+        table.foreign('recipe_id').references('recipes.id')
+      table.integer('ingredients_id').unsigned()
+        table.foreign('ingredients_id').references('ingredients.id')
     }),
     knex.schema.createTable('ingredients', table => {
       table.increments('id').primary()
-      table.string('ingredient')
-      table.string('description')
+      table.string('name')
+      table.text('description')
       table.timestamps(true, true)
     })
   ])
@@ -25,7 +29,8 @@ exports.up = function(knex) {
 
 exports.down = function(knex) {
   return Promise.all([
-    knex.schema.dropTable('ingedients'),
+    knex.schema.dropTable('recipe_ingredients'),
+    knex.schema.dropTable('ingredients'),
     knex.schema.dropTable('recipes')
   ])
 };
