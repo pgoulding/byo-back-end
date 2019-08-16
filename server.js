9b59f3c75de9ec4bbe40212fbdedbffa53a83f7a
Patrick Goulding <patrickgoulding@gmail.com>
2019-08-16 10:48:29 -0600

    add heroku dependencies

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const dbConnect = require('./connection')
+const environment = process.env.NODE_ENV || 'development'
+
 app.use(express.json());
 app.use(express.static('public'))
 app.use(cors())
